b7669605910041bd1783d020e176ff38cf4cdb94
Daniel <danieljayasutra@gmail.com>
2024-12-02 17:44:02 +0700

    update

--- audio.js
+++ audio.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const easy = require('./data/easy.json');
+const medium = require('./data/medium.json');
+const hard = require('./data/hard.json');
 
 router.post('/', (req, res, next) => {
   const difficulty = req.body.difficulty;
+  const excludedIds = req.body.excludedIds;
 
   if (difficulty == undefined) {
     return res.status(400).json({
@@ -11,7 +15,50 @@ router.post('/', (req, res, next) => {
     });
   }
 
-  res.status(200).json({});
+  if (difficulty !== 'easy' && difficulty !== 'medium' && difficulty !== 'hard') {
+    return res.status(400).json({
+      error: 'Bad Request',
+      message: 'Field "difficulty" should be easy, medium or hard',
+    });
+  }
+
+  if (!Array.isArray(excludedIds)) {
+    return res.status(400).json({
+      error: 'Bad Request',
+      message: 'Field "excludedIds" should be array of id',
+    });
+  }
+
+  let text;
+  if (difficulty === 'easy') {
+    text = getRandomData(easy, excludedIds);
+  }
+  if (difficulty === 'medium') {
+    text = getRandomData(medium, excludedIds);
+  }
+  if (difficulty === 'hard') {
+    text = getRandomData(hard, excludedIds);
+  }
+
+  
+
+  res.status(200).json({
+    text: text,
+  });
 });
 
 module.exports = router;
+
+function getRandomData(data, excludedIds) {
+  // Filter data to remove elements with IDs that are in excludedIds
+  const filteredData = data.filter((item) => !excludedIds.includes(item.id));
+
+  // If no data remains after the filter, return null.
+  if (filteredData.length === 0) {
+    return null;
+  }
+
+  // Randomly select elements from filtered data
+  const randomIndex = Math.floor(Math.random() * filteredData.length);
+  return filteredData[randomIndex];
+}
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const app = require('./app');
+require('dotenv').config();
+const fs = require('fs');
+
+// check .env
+if (!fs.existsSync('.env')) {
+  console.error('Error: .env file not found!');
+  process.exit(1); // Exit with status code 1 (failure)
+}
 
 // to host in local or vps
 const port = process.env.PORT || 3555;
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
-  console.log(process.env.NODE_ENV + " enviroment");
+  console.log(process.env.NODE_ENV + ' enviroment');
 });
