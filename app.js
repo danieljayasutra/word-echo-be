a9e9e41c3fc38bd2f9794cd6c86faf4023de2b80
danieljayasutra <danieljayasutra@gmail.com>
2024-12-03 23:02:19 +0700

    update

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const obtain = require('./quiz-obtain');
 const verify = require('./quiz-verify');
 
 const app = express();
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
+
 app.use(express.json({ limit: '1024kb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use('/status', (req, res) => {
